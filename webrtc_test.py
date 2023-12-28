4ddb8b36227e80ea165bc8d34b3246cd331241e6
jackkunjaja <155055166+jackkunjaja@users.noreply.github.com>
2023-12-28 19:10:53 +0900

    Add files via upload

--- webrtc_test.py
+++ webrtc_test.py
@@ -2,8 +2,17 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 #import cv2
 #import av #strealing video library
+#import logging
 
 st.title('Streamlit App Test')
 st.write('Hello world')
 
-webrtc_streamer(key='example')
+#st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+#st_webrtc_logger.setLevel(logging.WARNING)
+#st_webrtc_logger.setLevel(logging.INFO)
+
+#webrtc_streamer(key='example')
+webrtc_streamer(
+            key='example',
+            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+          )
